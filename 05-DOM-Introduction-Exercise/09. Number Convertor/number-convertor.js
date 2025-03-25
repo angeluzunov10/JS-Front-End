15c859ef4e3e13774e261ffa31a6416588feae79
angeluzunov10 <angel_d_u@abv.bg>
2025-03-25 21:00:49 +0200

    Made all the tasks for DOM Introduction Exercise

--- 05-DOM-Introduction-Exercise/09. Number Convertor/number-convertor.js
+++ 05-DOM-Introduction-Exercise/09. Number Convertor/number-convertor.js
@@ -1,3 +1,19 @@
 function solve() {
     // TODO
+
+    let number = Number(document.getElementById('input').value);
+
+    let convertTo = document.getElementById('selectMenuTo').value;
+
+    let result = '';
+
+    if (convertTo == 'binary'){
+        result = number.toString(2);
+    } else if (convertTo == 'hexadecimal'){
+        result = number.toString(16).toUpperCase();
+    }
+
+    let output = document.getElementById('result');
+    output.value = result;
+    
 }
