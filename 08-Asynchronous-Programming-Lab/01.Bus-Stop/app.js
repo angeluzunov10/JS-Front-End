75587ac50eaffe050988679a55ea7d5b7fe9e4a5
angeluzunov10 <angel_d_u@abv.bg>
2025-04-05 21:34:29 +0300

    Started the HTTP and AJAX Exercise

--- 08-Asynchronous-Programming-Lab/01.Bus-Stop/app.js
+++ 08-Asynchronous-Programming-Lab/01.Bus-Stop/app.js
@@ -1,3 +1,47 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    let stopId = document.getElementById('stopId').value;
+
+    let info;
+
+    let list = document.getElementById('buses');
+    list.replaceChildren();
+
+    try {
+        info = await getBusInfo(stopId);
+    } catch (err){
+        onError();
+        return
+    }
+
+    document.getElementById('stopName').textContent = info.name;
+
+    
+
+    for (let busId in info.buses) {
+        let item = document.createElement('li');
+        let time = info.buses[busId];
+
+        item.textContent = `Bus ${busId} arrives in ${time} minutes`;
+
+        list.appendChild(item);
+    }
+}
+
+function onError(){
+    document.getElementById('stopName').textContent = 'Error';
+
+} 
+
+async function getBusInfo(stopId) {
+    let url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    let res = await fetch(url);
+
+    if (res.status == 204){
+        throw new Error('No information for stop ' + stopId)
+    }
+
+    let data = await res.json();
+    
+    return data;
 }
